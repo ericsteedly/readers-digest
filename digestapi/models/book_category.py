c151a5ecb0ee48ae3c5a474b68a2cab850861930
Eric Steedly <ericsteedly@gmail.com>
2024-03-22 15:54:02 -0500

    Created reviews viewset and registered rout, seeded more data

--- digestapi/models/book_category.py
+++ digestapi/models/book_category.py
@@ -3,4 +3,4 @@ from django.db import models
 class BookCategory(models.Model):
     book = models.ForeignKey("Book", on_delete=models.CASCADE)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now_add=True)
--- digestapi/models/review.py
+++ digestapi/models/review.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Review(models.Model):
-    book = models.ForeignKey("Book", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="book_reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_reviews")
     rating = models.IntegerField()
     comment = models.CharField(max_length=400)
-    date_posted = models.DateField()
+    date_posted = models.DateField(auto_now_add=True)
